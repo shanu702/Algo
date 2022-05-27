0e059c0a07545e975d8c57c369057aef474f2167
shanu702 <43541045+shanu702@users.noreply.github.com>
2022-05-27 11:31:55 +0530

    Add files via upload

---
+++ String Permutation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation("dog");
+            Console.Read();
+        }
+
+        static void Permutation(string rest, string prefix = "")
+        {
+            if (string.IsNullOrEmpty(rest)) Console.WriteLine(prefix);
+
+            // Each letter has a chance to be per mutated
+            for (int i = 0; i < rest.Length; i++)
+            {
+                Permutation(rest.Remove(i, 1), prefix + rest[i]);
+            }
+        }
+    }
+}
